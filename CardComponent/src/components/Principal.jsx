f67c7d56bf61922b4a761520c3029f22d696f3bb
Ericka Ponce <erickaponce92@gmail.com>
2024-07-17 18:59:56 -0500

    add principal component and its styles

--- CardComponent/src/components/Principal.jsx
+++ CardComponent/src/components/Principal.jsx
@@ -1,3 +1,12 @@
+import '../styles/principal.css'
+
 export const Principal = () => {
-    
+
+    return (
+        <main className="main__container">
+            <div>sedans</div>
+            <div>suvs</div>
+            <div>luxury</div>
+        </main>
+    )
 }
