eea63ca456ac765f5e7c6d909dd25c82ffa3eab6
Ericka Ponce <erickaponce92@gmail.com>
2024-07-19 15:30:15 -0500

    add styles to components

--- CardComponent/src/components/Vehicle.jsx
+++ CardComponent/src/components/Vehicle.jsx
@@ -1,17 +1,15 @@
 import '../styles/vehicle.css'
 
-export const Vehicle = ({ svg, vehicleType, description, bgColor, bRadiusLeft}) => {
+export const Vehicle = ({ svg, vehicleType, description, bgColor, fontColor}) => {
     return (
         <div className={`autoContainer ${ bgColor }`} >
             <div className="autoContainer__type">
-                <picture className="autoContainer__type-img">
-                    <img src={ svg } alt="" />
-                </picture>
+                <img className="autoContainer__type-img" src={ svg } alt="" />
                 <h1 className="autoContainer__type-tittle">{ vehicleType }</h1>
                 <p className="autoContainer__type-par">{ description }</p>
             </div>
             <div className="autoContainer__button"> 
-                <button>
+                <button className={`${ fontColor }`}>
                     Learn More
                 </button>
             </div>
